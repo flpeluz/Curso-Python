82e703bdb2db2a5f6ab76be0300196324a1e43a2
Felipe Luz <felipeluz.me@gmail.com>
2025-11-26 13:27:26 -0300

    Update 09-exercicios-02.py

--- SEMANA 1/09-exercicios-02.py
+++ SEMANA 1/09-exercicios-02.py
@@ -1,3 +1,9 @@
+
+# Exercício:
+
+# Encontrar o preço de um produto digitado pelo usuário:
+
+
 preços = [1500, 1000, 800, 2000 ]
 lista_produtos = ["celular", "camera", "fone de ouvido", "monitor"]
 indice = 0
