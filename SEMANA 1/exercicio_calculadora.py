174972697896717cd7212a6268acad804609116a
Felipe Luz <felipeluz.me@gmail.com>
2025-11-28 18:18:20 -0300

    Update exercicio_calculadora.py

--- SEMANA 1/exercicio_calculadora.py
+++ SEMANA 1/exercicio_calculadora.py
@@ -1,27 +1,27 @@
-num1 = 0
-num2 = 0
-op = ""
-resultado = 0
+# num1 = 0
+# num2 = 0
+# op = ""
+# resultado = 0
 
-while True:
-    num1 = float(input("Digite o primeiro número: "))
-    op = input("Digite a operação: ")
-    num2 = float(input("Digite o segundo número: "))
+# while True:
+#     num1 = float(input("Digite o primeiro número: "))
+#     op = input("Digite a operação: ")
+#     num2 = float(input("Digite o segundo número: "))
 
-    if op == "+":
-        resultado = num1 + num2
+#     if op == "+":
+#         resultado = num1 + num2
 
-    elif op == "-":
-        resultado = num1 - num2
+#     elif op == "-":
+#         resultado = num1 - num2
 
-    elif op == "*":
-        resultado = num1 * num2
+#     elif op == "*":
+#         resultado = num1 * num2
 
-    elif op == "/":
-        resultado = num1 / num2
+#     elif op == "/":
+#         resultado = num1 / num2
 
-    else:
-        print("Operação Inválida!")
+#     else:
+#         print("Operação Inválida!")
 
-    print(f"{num1} {op} {num2} = {resultado}")
-    break
+#     print(f"{num1} {op} {num2} = {resultado}")
+#     break
