7a5ebfd74a97baa50960ac9d189c2e6236ace1f7
Felipe Luz <felipeluz.me@gmail.com>
2025-12-06 11:03:28 -0300

    Update 13-def.py

--- SEMANA 1/13-def.py
+++ SEMANA 1/13-def.py
@@ -3,3 +3,28 @@
 # Função -> Bloco de código reutilizável
 # Organização
 
+# Como funciona:
+    # def nome_da_funcao(parametros):
+        # instrução1
+        # instrução2
+        # ...
+        # return valor (opcional)
+
+def calcular_imposto(valor):
+    if valor <= 1000:
+        imposto = valor * 0.1
+    elif valor <= 2000:
+        imposto = valor * 0.13
+    else:
+        imposto = valor * 0.2
+
+    return imposto
+
+preço_produto1 = 2500
+preçoproduto2 = 3500
+
+imposto_produto1 = calcular_imposto(preço_produto1)
+imposto_produto2 = calcular_imposto(preçoproduto2)
+
+print(imposto_produto1)
+print(imposto_produto2)
